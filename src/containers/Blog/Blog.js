f00ca6c63aebad704f95516b7cd63f499d6a6200
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-12-18 22:48:53 -0500

    extracting route paramteres

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,8 @@ import "./Blog.module.css";
 import Posts from "./Posts/Posts";
 import NewPost from './NewPost/NewPost';
 
+import FullPost from './FullPost/FullPost';
+
 class Blog extends Component {
   render() {
     return (
@@ -39,6 +41,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
       </div>
     );
   }
