d074544365cd4d335656ad91d46a7f38abd6c3c3
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-12-18 22:54:24 -0500

    switch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 // import axios from 'axios';
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 import "./Blog.module.css";
 
 import Posts from "./Posts/Posts";
-import NewPost from './NewPost/NewPost';
+import NewPost from "./NewPost/NewPost";
 
-import FullPost from './FullPost/FullPost';
+import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
   render() {
@@ -18,10 +18,16 @@ class Blog extends Component {
             <ul>
               <li>
                 <NavLink
-                to="/"
-                exact
-                activeClassName="my-active"
-                activeStyle={{color:'#fa923f', textDecoration:'underline' }}>Home</NavLink>
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline"
+                  }}
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
                 <NavLink
@@ -39,9 +45,11 @@ class Blog extends Component {
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
